d433af982803fa246ebbc7facecd88c6c3b4cd0d
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-10-24 08:11:29 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let puesto: number = 4;
+
+if (puesto == 1) {
+  console.log("medalla de oro");
+} else {
+  if (puesto == 2) {
+    console.log("medalla de plata");
+  } else {
+    if (puesto == 3) {
+      console.log("medalla de bronse");
+    } else {
+      console.log("medalla de participacion");
+    }
+  }
+}
